b0bfe69832f13c5516b34527a58304e685dd5e3e
lucas.padueli <lucascapellapadueli@gmail.com>
2023-06-13 02:10:48 -0300

    atualizado

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -15,18 +15,7 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-// adicionar uma receita ao usuário
-router.post('/add-incomes/:incomeId', isAuthenticated, async (req, res, next) => {
-    const { incomeId } = req.params;
-    const userId = req.payload._id;
-    try {
-      const userFromDB = await User.findByIdAndUpdate(userId, { $push: { incomes: incomeId } }, { new: true });
-      res.status(200).json(userFromDB);
-    } catch (error) {
-      next(error);
-    }
-  });
-  
+
   
   router.get('/profile', async (req, res, next) => {
     const userId = req.payload._id;
